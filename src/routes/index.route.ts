05386f40b0184d0c272766ef842514673076bd91
Alao Abiodun <abiodundev@gmail.com>
2024-02-03 09:26:43 +0100

    chore:set up webhook POST route

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import taskRoute from './task.route';
+import webhookRoute from './webhook.route';
 
 const router = Router();
 
 taskRoute(router);
+webhookRoute(router);
 
 export default router;
---
+++ src/routes/webhook.route.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import {
+    webhook
+} from '../controllers/webhook.controller';
+
+export default (router: Router) => {
+    router.post('/webhook', webhook);
+};
